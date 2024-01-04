2c7b9c06e5a86bffbce71a5467b6405c066ab8ed
Beatriz <beatrizsouzasantos1288@gmail.com>
2024-01-04 18:20:00 -0300

    add images

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 import { styles } from './styles';
 
 export function SignIn(){
+  //armazena valor
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}> 
       <Text>Hi Beatriz !</Text>
+
+      <TextInput 
+      style={styles.input}
+      //mostra o valor
+      onChangeText={setText}
+      />
+
+      <Text>
+        Você digitou: { Text }
+      </Text>
+
     </View>
   );
 }
